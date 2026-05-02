aa82d9a1020924a978a2a8a0aa115e672d8274d6
Hotaru <xyz21079@outlook.com>
2026-05-02 19:44:19 +0800

    1

--- src/views/user/User.vue
+++ src/views/user/User.vue
@@ -1,11 +1,22 @@
 <template>
-  <h1>111</h1>
+  <MapContainer> </MapContainer>
 </template>
 
 <script lang="ts" setup>
+import request from "../../utils/request";
+import MapContainer from "../login/MapContainer.vue";
+import { onMounted, ref } from "vue";
 
+const EnList = ref([]);
+
+const getAllEnList = async () => {
+  const res = await request.get("/userGetAllEn");
+  EnList.value = res.data ?? [];
+};
+
+onMounted(() => {
+  void getAllEnList;
+});
 </script>
 
-<style>
-
-</style>
+<style></style>
